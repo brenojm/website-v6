2f44f4dd543a5e482b0d5f84df128afd79e1f9ae
Arpan Neupane <arpanneupane19@gmail.com>
2023-06-01 18:53:28 -0500

    experience section complete

---
+++ app/components/Experience.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React from "react";
+const Fade = require("react-reveal/Fade");
+import { experiences } from "../utils/experienceList";
+import Card from "./Card";
+
+function Experience() {
+  return (
+    <section id="experience" className="w-full min-h-screen flex items-center">
+      {/* experience display */}
+
+      <div className="w-full flex justify-center lg:pt-0 pt-12">
+        <div className="flex-1">
+          <Fade left>
+            <h2 className="lg:mb-16 mb-8 font-bold lg:text-5xl text-3xl underline underline-offset-8 text-center">
+              experience
+            </h2>
+          </Fade>
+          <Fade right>
+            <div className="flex justify-center items-center flex-wrap">
+              {experiences.map((experience, index) => (
+                <Card
+                  key={index}
+                  company={experience.company}
+                  position={experience.position}
+                  time={experience.time}
+                  image={experience.image}
+                  width={experience.width}
+                  height={experience.height}
+                />
+              ))}
+            </div>
+          </Fade>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Experience;
