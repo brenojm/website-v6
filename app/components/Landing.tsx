93774602eebee2ca4fe6132165ed7de50d301dda
Arpan Neupane <arpanneupane19@gmail.com>
2023-06-01 18:53:18 -0500

    changed react reveal

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -3,9 +3,9 @@
 import React from "react";
 import { Typewriter } from "react-simple-typewriter";
 import Image from "next/image";
-import { routes } from "./routes";
+import { routes } from "../utils/routes";
 import { BsArrowRight } from "react-icons/bs";
-const Bounce = require("react-reveal/Bounce");
+const Fade = require("react-reveal/Fade");
 
 function Landing() {
   const words = [
@@ -24,7 +24,7 @@ function Landing() {
       className="w-full min-h-screen flex lg:flex-row lg:justify-between justify-center flex-col-reverse items-center"
     >
       {/* introduction */}
-      <Bounce left>
+      <Fade left>
         <div className="intro flex flex-col lg:text-left text-center">
           <div className="intro-name">
             <h1 className="lg:text-2xl text-lg font-light">hi! i am</h1>
@@ -61,19 +61,19 @@ function Landing() {
             ))}
           </div>
         </div>
-      </Bounce>
+      </Fade>
 
       {/* gif */}
-      <Bounce right>
+      <Fade right>
         <div>
           <Image
             src="/programmer-gif.gif"
-            alt="Programmer Gif"
-            width={475}
-            height={100}
+            alt="Home Gif"
+            width={505}
+            height={505}
           />
         </div>
-      </Bounce>
+      </Fade>
     </section>
   );
 }
