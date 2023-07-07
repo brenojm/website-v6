7bd27f44036b7568264adad9aa517421ae7038ab
Arpan Neupane <arpanneupane19@gmail.com>
2023-07-07 14:31:59 -0500

    added project modal to card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,10 +1,26 @@
 import React, { useState } from "react";
 import { Project } from "@/utils/projectsList";
 import Image from "next/image";
+import Modal from "./Modal";
 
 function ProjectCard(props: Project) {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
   return (
-    <div className="p-5 mx-3 mb-6 flex flex-col justify-center items-center rounded-md shadow-lg">
+    <div
+      className="p-5 mx-3 mb-6 flex flex-col justify-center items-center rounded-md shadow-lg cursor-pointer"
+      onClick={!modalVisible ? toggleModal : () => null}
+    >
+      {modalVisible ? (
+        <div
+          className="w-screen h-screen fixed z-[100] left-0 top-0 bg-modalBackdrop cursor-default"
+          onClick={toggleModal}
+        ></div>
+      ) : null}
       <div className="text-xl mb-2 text-fgText">{props.title}</div>
       <div className="mb-4">
         <Image
@@ -27,6 +43,15 @@ function ProjectCard(props: Project) {
           </div>
         ))}
       </div>
+
+      <Modal
+        title={props.title}
+        description={props.desc}
+        gitLink={props.gitLink}
+        projectLink={props.projectLink}
+        toggleModal={toggleModal}
+        modalVisible={modalVisible}
+      />
     </div>
   );
 }
